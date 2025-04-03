656411474168917c54f91f36c8d3f5a8119087f2
Mohammed Hassan <en.mhdhassan@outlook.com>
2025-04-03 16:17:36 +0200

    working Simulate class + cleaning

--- UnitTests/cxx/main.cpp
+++ UnitTests/cxx/main.cpp
@@ -48,30 +48,5 @@ int main(int argc, char **argv)
     simulate.addParticles(poly);
     simulate.solve(3*0.0001);
 
-    // simulate.run(0.001);
-    // ContactDetection<Spherical> cd("input.json");
-    // auto potential_pairs = cd.broadPhase(particles);
-    // auto actual_contacts = cd.narrowPhase(particles, potential_pairs);
-
-    // Output output("results");
-    // output.writeParticles(particles, 0);
-    // output.writeParticles(poly, 0);
-    // output.writeTree(cd.getTree(),0);
-
-    // std::cout << "particle radius: "<< particles[0].getRadius() << std::endl;
-    // std::cout << "particle Material id: "<< particles[0].getMaterialId() << std::endl;
-    // const Spherical* devParticle =  particles.data();
-
-    // GpuClass gpu(devParticle, N);
-    // gpu.printHellow();
-
-    // cudaDeviceReset causes the driver to clean up all state. While
-    // not mandatory in normal operation, it is good practice.  It is also
-    // needed to ensure correct operation when the application is being
-    // profiled. Calling cudaDeviceReset causes all profile data to be
-    // flushed before the application exits
-
-    // cudaDeviceReset();
-    // exit(bTestResult ? EXIT_SUCCESS : EXIT_FAILURE);
     return 0;
 }
